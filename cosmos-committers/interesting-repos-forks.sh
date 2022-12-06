4b1f9a3f1b6a9bedd6e081b6e724257db5109d6e
Manfred Touron <94029+moul@users.noreply.github.com>
2022-12-06 19:25:55 +0100

    feat: add interesting-repos-forks.sh + results for cosmos-sdk
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

---
+++ cosmos-committers/interesting-repos-forks.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Usage:   ./interesting-repos-forks.sh REPO
+# Example: ./interesting-repos-forks.sh cosmos/cosmos-sdk
+# Requirements: 'hub'
+#
+# This script lookups existing forks of a repo and create a CSV file with main informations.
+
+
+REPO=$1
+
+echo "full_name,size,created_at,updated_at,pushed_at,open_issues,stars,watchers"
+hub api "repos/${REPO}/forks" --paginate | \
+    jq -r '.[] | [
+       .full_name,
+       (.size|tostring),
+       .created_at,
+       .updated_at,
+       .pushed_at,
+       (.open_issues_count|tostring),
+       (.forks_count|tostring),
+       (.stargazers_count|tostring),
+       (.watchers_count|tostring)
+     ] | join(",")'
